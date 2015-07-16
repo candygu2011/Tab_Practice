f9f0acb3693542f7bf1f1c3411fc1d1801d47b48
candygu2011 <candygu2011@gmail.com>
2015-07-16 20:08:49 +0800

    Test iOS7 UITableviewcell Delete

---
+++ ScrollerVC/DeleteTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeleteTableViewController.h
+//  ScrollerVC
+//
+//  Created by hi on 15/7/16.
+//  Copyright (c) 2015年 GML. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeleteTableViewController : UITableViewController
+
+@end
---
+++ ScrollerVC/DeleteTableViewController.m
@@ -0,0 +1,101 @@
+//
+//  DeleteTableViewController.m
+//  ScrollerVC
+//
+//  Created by hi on 15/7/16.
+//  Copyright (c) 2015年 GML. All rights reserved.
+//
+
+#import "DeleteTableViewController.h"
+
+@interface DeleteTableViewController ()
+
+@property (nonatomic, retain) NSMutableArray *dataArray;
+
+
+@end
+
+@implementation DeleteTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [super viewDidLoad];
+    _dataArray = [[NSMutableArray alloc] initWithObjects:@"数据11",@"数据12",@"数据13",@"数据14",@"数据15",nil];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return _dataArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = [_dataArray objectAtIndex:indexPath.row];
+    return cell;
+
+}
+
+
+
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    return YES;
+}
+
+
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [_dataArray removeObjectAtIndex:indexPath.row];
+
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        
+    }
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+
+    
+    }
+}
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
